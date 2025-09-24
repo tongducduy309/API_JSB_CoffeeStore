f0469e678a7f391bc79fe482350cac417b83a05b
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2025-09-25 00:39:36 +0700

    update init data

--- src/main/java/com/api_coffee_store/api_coffee_store/database/database.java
+++ src/main/java/com/api_coffee_store/api_coffee_store/database/database.java
@@ -24,38 +24,39 @@ public class database {
         return new CommandLineRunner() {
             @Override
             public void run(String... args) throws Exception {
-                Category categoryA =  Category.builder()
-                        .name("Trà sữa")
-
-                        .build();
-                log.info("Insert Category",categoryRepository.save(categoryA));
-                User userA = User.builder()
-                        .email("user@test.com")
-                        .password(passwordEncoder.encode("12345678"))
-                        .fullname("User")
-                        .build();
-                userRepository.save(userA);
-                Product productA = Product.builder()
-                        .name("Trà Sữa Phúc Long")
-                        .variants(new ArrayList<>())
-                        .description("Béo Ngọt")
-                        .category(categoryA)
-                        .build();
-                Product productB = Product.builder()
-                        .name("Trà Sữa Phúc Long B")
-                        .variants(new ArrayList<>())
-                        .description("Béo Ngọt")
-                        .category(categoryA)
-                        .build();
-                log.info("Insert Product",productRepository.save(productB));
-                Product productC = Product.builder()
-                        .name("Trà Sữa Phúc Long C")
-                        .variants(new ArrayList<>())
-                        .description("Béo Ngọt")
-                        .category(categoryA)
-                        .status(false)
-                        .build();
-                log.info("Insert Product",productRepository.save(productC));
+//                Category categoryA =  Category.builder()
+//                        .name("Trà sữa")
+//
+//                        .build();
+//                log.info("Insert Category",categoryRepository.save(categoryA));
+//                User userA = User.builder()
+//                        .email("user@test.com")
+//                        .password(passwordEncoder.encode("12345678"))
+//                        .fullname("User")
+//                        .build();
+//                userRepository.save(userA);
+////                Product productA = Product.builder()
+////                        .name("Trà Sữa Phúc Long")
+////                        .variants(new ArrayList<>())
+////                        .description("Béo Ngọt")
+////                        .category(categoryA)
+////                        .build();
+////                log.info("Insert Product",productRepository.save(productA));
+//                Product productB = Product.builder()
+//                        .name("Trà Sữa Phúc Long B")
+//                        .variants(new ArrayList<>())
+//                        .description("Béo Ngọt")
+//                        .category(categoryA)
+//                        .build();
+//                log.info("Insert Product",productRepository.save(productB));
+//                Product productC = Product.builder()
+//                        .name("Trà Sữa Phúc Long C")
+//                        .variants(new ArrayList<>())
+//                        .description("Béo Ngọt")
+//                        .category(categoryA)
+//                        .status(false)
+//                        .build();
+//                log.info("Insert Product",productRepository.save(productC));
 
 //                Cart cart = Cart.builder()
 //                        .product(productB)
